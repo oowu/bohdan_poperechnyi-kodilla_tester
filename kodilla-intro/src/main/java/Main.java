613926e3689a86035e4d79e8e75ca994cc51247f
Bohdan Poperechnyi <pbohdanpl@gmail.com>
2022-07-01 22:53:05 +0200

    Main and MainExtended added

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Bohdan";
+        int myAge = 27;
+        double height = 1.85;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String carMake = "Honda";
+        String carModel = "Civic";
+        double carEngine = 1.8;
+        char carVersion = 'B';
+        boolean isNew = false;
+        int carYear = 2011;
+        System.out.println(carMake);
+        System.out.println(carModel);
+        System.out.println(carEngine);
+        System.out.println(carVersion);
+        System.out.println(isNew);
+        System.out.println(carYear);
+    }
+}
