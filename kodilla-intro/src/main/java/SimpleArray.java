97be93774a95d0e8d41d0980445579ada2081f3d
Bohdan Poperechnyi <pbohdanpl@gmail.com>
2022-07-04 17:30:32 +0200

    SimpleArray added

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,9 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] programmingLanguages = new String[] {"Java", "Python", "C", "Go", "Ruby"};
+        String randElem = programmingLanguages[3];
+        int numOfElem = programmingLanguages.length;
+        System.out.println(randElem);
+        System.out.println("Number of elements: " + numOfElem);
+    }
+}
